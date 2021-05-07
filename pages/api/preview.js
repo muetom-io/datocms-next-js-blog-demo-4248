498f3a6583ce9552429789cd3464cbba5e4af248
rotpunkt-dev <rotpunkt.dev@gmail.com>
2021-05-07 22:23:52 +0200

    removed path

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -23,6 +23,6 @@ export default async (req, res) => {
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: `/${post.slug}` })
+  res.writeHead(307, { Location: `/` })
   res.end()
 }
