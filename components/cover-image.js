ce34a9a88dfbbf7728daa9d8a82e1922a6ac6e82
rotpunkt-dev <rotpunkt.dev@gmail.com>
2021-05-19 08:32:24 +0200

    cleanup classname

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,16 +1,12 @@
 import { Image } from "react-datocms";
-import cn from "classnames";
-
-export default function CoverImage({ title, responsiveImage, slug }) {
+export default function CoverImage({ title, responsiveImage }) {
   const image = (
     <Image
       data={{
         ...responsiveImage,
         alt: `Cover Image for ${title}`,
       }}
-      className={cn("shadow-small", {
-        "hover:shadow-medium transition-shadow duration-200": slug,
-      })}
+      className="shadow-small"
     />
   );
   return <div className="sm:mx-0">{image}</div>;
