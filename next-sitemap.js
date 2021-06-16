e543319cb76e12bbb9567bc388412da44f614f74
rotpunkt-dev <rotpunkt.dev@gmail.com>
2021-06-16 08:30:27 +0200

    changed url to www

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://unser-kalamata-olivenoel.de',
+    siteUrl: process.env.SITE_URL || 'https://www.unser-kalamata-olivenoel.de',
     generateRobotsTxt: true, // (optional)
     // ...other options
   }
